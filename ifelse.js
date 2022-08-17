ace62ddbf219abf5eab1a266e9b9f03f8fec7dd2
pavan <Pavanpk123@gmail.com>
2022-08-17 16:04:46 +0530

    if else condition statement

---
+++ ifelse.js
@@ -0,0 +1,13 @@
+var a=500;
+if (x=10){
+    console.log('if else condition a');
+}
+else if(x >10 && x>=50){
+    console.log('if else condition1 b');
+}
+else if (x>50 && x<=122){
+    console.log('if else conditon2 c');
+}
+else {
+    console.log('you get nothing');
+}
